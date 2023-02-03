7a03c1ab5cd368bcf18b7ea1f374f4afb38222d6
Yair Beoyer <36797112+yairbr01@users.noreply.github.com>
2023-02-03 12:38:18 +0200

    Create redirect_all_site_to_login_page.php

---
+++ redirect_all_site_to_login_page.php
@@ -0,0 +1,12 @@
+<?php
+// Redirect all site to custom login page
+
+add_action( 'template_redirect', 'yair_redirect' );
+function yair_redirect() {
+	if ( ! is_page( 'login' ) && get_current_user_id() == 0 ) {
+		wp_redirect( home_url() . '/login/', 301 ); 
+    	exit;    	
+	} 
+}
+
+?>
