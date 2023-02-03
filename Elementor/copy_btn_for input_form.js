686d85de51dd3e6fec2f32e901fbba50a2eb4737
Yair Beoyer <36797112+yairbr01@users.noreply.github.com>
2023-02-03 05:23:41 +0200

    Create copy_btn_for input_form.js

---
+++ Elementor/copy_btn_for input_form.js
@@ -0,0 +1,15 @@
+<script>
+
+function CopyUrlFromInput() {
+    var copyText = document.getElementById("form-field-url");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+    
+    document.querySelector("#copy-btn span").innerHTML = "הועתק";
+    document.querySelector("#copy-btn span").style.color = "#BB0205";
+}
+document.getElementById("copy-btn").addEventListener("click", CopyUrlFromInput);
+document.getElementById("form-field-url").addEventListener("click", CopyUrlFromInput);
+
+</script>
