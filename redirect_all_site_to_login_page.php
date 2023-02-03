e6d523333f77121cbb24c1d573ffec274099a20e
Yair Beoyer <36797112+yairbr01@users.noreply.github.com>
2023-02-03 12:38:37 +0200

    Update redirect_all_site_to_login_page.php

--- redirect_all_site_to_login_page.php
+++ redirect_all_site_to_login_page.php
@@ -5,7 +5,7 @@ add_action( 'template_redirect', 'yair_redirect' );
 function yair_redirect() {
 	if ( ! is_page( 'login' ) && get_current_user_id() == 0 ) {
 		wp_redirect( home_url() . '/login/', 301 ); 
-    	exit;    	
+    		exit;    	
 	} 
 }
 
