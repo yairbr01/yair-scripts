d68bbbd7e3547cbe163bc9e7c50960b7b44f61e2
Yair Beoyer <36797112+yairbr01@users.noreply.github.com>
2023-02-03 11:53:42 +0200

    Create get_current_page_url.php

---
+++ get_current_page_url.php
@@ -0,0 +1,11 @@
+<?php
+// Shortcode for current page
+
+add_shortcode ('get_current_page_url', 'current_page_url');
+function current_page_url() {
+  $pageURL = 'https://';
+	$pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+	return $pageURL;
+}
+
+?>
