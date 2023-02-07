c7e811e162cd71646307baf2d3c62c5f5e8a281a
Yair Beoyer <36797112+yairbr01@users.noreply.github.com>
2023-02-07 03:19:39 +0200

    Create shortcode_my_orders.php

---
+++ WooCommerce/shortcode_my_orders.php
@@ -0,0 +1,11 @@
+<?php
+// Shortcode for get just "my orders" pat of "my account".
+
+function custom_my_orders_shortcode() {
+   ob_start();
+   wc_get_template( 'myaccount/my-orders.php', array( 'current_user' => get_user_by( 'id', get_current_user_id() ) ) );
+   return ob_get_clean();
+}
+add_shortcode( 'get_my_orders', 'custom_my_orders_shortcode' );
+
+?>
